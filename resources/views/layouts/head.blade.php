aea55beb62c3ade169b0fac9eb2e1f936497337f
TaranggaWana20051214068 <taranggawana.20068@mhs.unesa.ac.id>
2024-09-20 02:14:13 +0700

    logo fix4

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,7 +6,7 @@
 
 <!-- PWA  -->
 <meta name="theme-color" content="#6777ef" />
-<link rel="apple-touch-icon" href="{{ asset('images/logo.png') }}">
+<link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
 <link rel="manifest" href="{{ asset('/manifest.json') }}">
 
 
